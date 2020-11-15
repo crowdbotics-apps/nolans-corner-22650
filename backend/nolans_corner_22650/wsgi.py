0f794f94c8e45994f8ba08e301be9883d299823d
Crowdbotics <team@crowdbotics.com>
2020-11-15 15:04:54 +0000

    Initial commit.

---
+++ backend/nolans_corner_22650/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for nolans_corner_22650 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nolans_corner_22650.settings')
+
+application = get_wsgi_application()
