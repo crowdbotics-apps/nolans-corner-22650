4bdec155cc6675b3704c43b9602c528a0ff0b39f
Crowdbotics <team@crowdbotics.com>
2020-11-15 15:09:20 +0000

    Update models

--- backend/nolans_corner_22650/wsgi.py
+++ backend/nolans_corner_22650/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nolans_corner_22650.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "nolans_corner_22650.settings")
 
 application = get_wsgi_application()
